f33c9241595ea4864f9bd9faa327017de35ff002
Chris Long <chris.long@mac.com>
2015-10-28 15:03:29 -0700

    Initial Commit

---
+++ qtascii/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  qtascii
+//
+//  Created by Chris Long on 10/28/15.
+//  Copyright © 2015 nito. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
